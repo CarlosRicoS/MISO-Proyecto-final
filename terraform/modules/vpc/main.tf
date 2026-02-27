afc9fb444a171d02cd35b2ced02b9ee3311b0c46
Angel Henao <ay.henao@uniandes.edu.co>
2026-02-26 20:32:53 -0500

    feat(terraform): add initial Terraform configuration for VPC setup
    * Define AWS provider and required variables
    * Create VPC with public and private subnets
    * Set up security groups and route tables
    * Include outputs for VPC ID and security group ID

---
+++ terraform/modules/vpc/main.tf
@@ -0,0 +1,180 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.17.0"
+    }
+  }
+}
+data "aws_availability_zones" "all" {}
+
+locals {
+  availability_zones = length(var.availability_zones) > 0 ? var.availability_zones : data.aws_availability_zones.all.names
+
+  vpc_default_egress_rules = [
+    {
+      from_port       = 0
+      to_port         = 0
+      protocol        = "-1"
+      cidr_blocks     = ["0.0.0.0/0"]
+      security_groups = []
+      self            = false
+      description     = "Default VPC egress rule"
+    }
+  ]
+
+  vpc_default_ingress_rules = [
+    {
+      from_port       = 0
+      to_port         = 0
+      protocol        = "-1"
+      cidr_blocks     = [var.vpc_cidr]
+      security_groups = []
+      self            = false
+      description     = "Default VPC ingress rule"
+    }
+  ]
+}
+
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  tags = {
+    name = "${var.project_name}-vpc"
+  }
+}
+
+# Public Subnets
+resource "aws_subnet" "public" {
+  count = length(var.public_subnets)
+
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = element(var.public_subnets, count.index)
+  availability_zone       = element(local.availability_zones, count.index)
+  map_public_ip_on_launch = true
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    is_public = true
+  }
+}
+
+resource "aws_internet_gateway" "vpc" {
+  count  = length(var.public_subnets) > 0 ? 1 : 0
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.project_name}-internet_gateway"
+  }
+}
+
+resource "aws_route_table" "public" {
+  count  = length(var.public_subnets) > 0 ? 1 : 0
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    is_public = true
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnets)
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public[0].id
+}
+
+resource "aws_route" "public_internet_gateway" {
+  count                  = length(var.public_subnets) > 0 ? 1 : 0
+  route_table_id         = aws_route_table.public[0].id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.vpc[0].id
+}
+
+# Private Subnets
+resource "aws_subnet" "private" {
+  count = length(var.private_subnets)
+
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.private_subnets, count.index)
+  availability_zone = element(local.availability_zones, count.index)
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route_table" "private" {
+  count  = length(var.private_subnets) > 0 ? 1 : 0
+  vpc_id = aws_vpc.main.id
+}
+
+
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnets)
+
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[0].id
+}
+
+
+resource "aws_eip" "nat" {
+  count  = 1
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "vpc" {
+  count         = 1
+  subnet_id     = aws_subnet.public[count.index].id
+  allocation_id = aws_eip.nat[count.index].id
+}
+
+resource "aws_route" "private_nat" {
+  count                  = 1
+  route_table_id         = aws_route_table.private[0].id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.vpc[0].id
+}
+
+# VPC Security Group
+resource "aws_security_group" "vpc" {
+  name        = "vpc-securty-group-${var.project_name}"
+  description = "Managed by Terraform"
+  vpc_id      = aws_vpc.main.id
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  dynamic "ingress" {
+    for_each = local.vpc_default_ingress_rules
+    content {
+      from_port       = ingress.value.from_port
+      to_port         = ingress.value.to_port
+      protocol        = ingress.value.protocol
+      cidr_blocks     = ingress.value.cidr_blocks
+      security_groups = ingress.value.security_groups
+      self            = ingress.value.self
+      description     = ingress.value.description
+    }
+  }
+
+  dynamic "egress" {
+    for_each = local.vpc_default_ingress_rules
+    content {
+      from_port       = egress.value.from_port
+      to_port         = egress.value.to_port
+      protocol        = egress.value.protocol
+      cidr_blocks     = egress.value.cidr_blocks
+      security_groups = egress.value.security_groups
+      self            = egress.value.self
+      description     = egress.value.description
+    }
+  }
+}
+
+
