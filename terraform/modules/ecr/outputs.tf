278a7071d4c1cb2c2c1f82c2842970d86f8fd36f
Angel Henao <ay.henao@uniandes.edu.co>
2026-02-27 00:20:44 -0500

    feat(terraform): add repository URL output to ECR config
    * Added output for repository URL in outputs.tf
    * Provides easier access to the ECR repository URL

--- terraform/modules/ecr/outputs.tf
+++ terraform/modules/ecr/outputs.tf
@@ -2,3 +2,8 @@ output "repository_arn" {
   value       = aws_ecr_repository.main.arn
   description = "Repository ARN."
 }
+
+output "repository_url" {
+  value       = aws_ecr_repository.main.repository_url
+  description = "Repository URL."
+}
