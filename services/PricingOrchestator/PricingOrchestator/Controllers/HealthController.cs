290c463b57b485ec4bfbff2d2e21d81dcb166fb0
Andres Ramirez <ha.ramirezg1@uniandes.edu.co>
2026-03-07 22:05:39 -0500

    Adjusted PricingOrchestator

---
+++ services/PricingOrchestator/PricingOrchestator/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PricingOrchestator.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class HealthController : ControllerBase
+	{
+		[HttpGet()]
+		public IResult GetHealth()
+		{
+			return Results.Ok();
+		}
+	}
+}
---
+++ services/PricingOrchestator/PricingOrchestator/Models/Property.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PricingOrchestator.Models
+{
+	public class Property
+	{
+		[JsonPropertyName("id")]
+		public Guid Id { get; set; }
+
+		[JsonPropertyName("name")]
+		public string Name { get; set; }
+
+		[JsonPropertyName("maxCapacity")]
+		public int MaxCapacity { get; set; }
+
+		[JsonPropertyName("description")]
+		public string Description { get; set; }
+
+		[JsonPropertyName("urlBucketPhotos")]
+		public string UrlBucketPhotos { get; set; }
+
+		[JsonPropertyName("checkInTime")]
+		public TimeOnly CheckInTime { get; set; }
+
+		[JsonPropertyName("checkOutTime")]
+		public TimeOnly CheckOutTime { get; set; }
+
+		[JsonPropertyName("adminGroupId")]
+		public string AdminGroupId { get; set; }
+
+		[JsonPropertyName("price")]
+		public decimal Price { get; set; }
+
+	}
+}
