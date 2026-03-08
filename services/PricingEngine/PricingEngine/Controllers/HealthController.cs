d43eb9946df17fd14bad4f64027a0ee9203db5e0
Andres Ramirez <ha.ramirezg1@uniandes.edu.co>
2026-03-07 22:02:10 -0500

    added health endpoint

---
+++ services/PricingEngine/PricingEngine/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PricingEngine.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class HealthController : ControllerBase
+	{
+		[HttpGet()]
+		public IResult GetHealth()
+		{
+			return Results.Ok();
+		}
+	}
+}
