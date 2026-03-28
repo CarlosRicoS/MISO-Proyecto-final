72c6b25482fe9131ff13e357ca342cd26e6fd123
Hector Andres Ramirez <ha.ramirezg1@uniandes.edu.co>
2026-03-27 21:01:00 -0500

    Added support for Android (#64)

---
+++ user_interface/android/app/src/main/java/com/uniandes/travelhub/MainActivity.java
@@ -0,0 +1,5 @@
+package com.uniandes.travelhub;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ user_interface/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.uniandes.travelhub.app',
+  appName: 'travelhub',
+  webDir: 'www'
+};
+
+export default config;
